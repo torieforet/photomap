20697cb5285b409385aa8e722c50cfda81b94fe7
torieforet <77910527+torieforet@users.noreply.github.com>
2021-04-11 14:24:25 -0500

    Update photomap.js

--- photomap.js
+++ photomap.js
@@ -14,14 +14,14 @@
  */
 function createPhotoMap () {
   // URL of a Google Sheets spreadsheet output as CSV
-  var csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSuESp280YE8_PokNYJSK_V3zxGh7ZJlDqMNeliCoz1kt6q2ii7lRovZ-yMlEFY2n6rhUu3vOpnvFGx/pub?gid=0&single=true&output=csv';
+  var csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQHiYy6yzfFeFu9PD_lw-PBJXimAWgluv4oBffo9yWF-Ewsh1sAFrip3c6Oza4iib3RwP_yVrJrt-uD/pub?gid=0&single=true&output=csv';
   
   // create map object with center lat/lon and zoom level
   var map = L.map('map').setView([30.44, -91.187], 13);
   
   // create basemap object. See examples at https://leaflet-extras.github.io/leaflet-providers/preview/
-  var basemap = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
-  	maxZoom: 16,
+  var basemap = L.tileLayer('https://services.arcgisonline.com/arcgis/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}}', {
+  	maxZoom: 4,
   	attribution: 'USGS'
   }).addTo(map);
   
